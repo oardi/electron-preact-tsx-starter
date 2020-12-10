f248c1aa6703df8a8403a5736cbd3c0c43acac21
Ardian Shala <ardian.sh@outlook.de>
2020-12-10 21:35:26 +0100

    init

---
+++ readme.md
@@ -0,0 +1,19 @@
+# Electron Forge Preact TSX Starter
+
+## Usage
+`npm start`
+
+
+## Build
+`npm run make`
+or
+`npm run package`
+
+
+## Included
+
+* electron
+* electron-forge
+* preact
+* scss
+* twitter bootstrap
---
+++ webpack.renderer.config.js
@@ -0,0 +1,47 @@
+
+const rules = require('./webpack.rules');
+
+rules.push({
+	test: /\.tsx?$/,
+	loader: ['ts-loader']
+});
+
+rules.push({
+	test: /\.css$/,
+	use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+});
+
+rules.push({
+	test: /\.s[ac]ss$/i,
+	use: [
+		'style-loader',
+		'css-loader',
+		'sass-loader',
+	],
+})
+
+rules.push({ test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/, loader: 'file-loader?name=assets/[name].[ext]' })
+
+rules.push({
+	test: /\.(png|jpg|gif)$/,
+	use: [
+		{
+			loader: 'file-loader',
+			options: {
+				name: '[name].[ext]',
+				outputPath: 'assets/img',
+				publicPath: 'assets/img'
+			}
+		}
+	]
+})
+
+module.exports = {
+	// Put your normal webpack config below here
+	module: {
+		rules,
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.jsx']
+	},
+};
